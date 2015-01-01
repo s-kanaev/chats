fa19fa86c8155254d3bb645dbe705a4787d4ac34
Sergy Kanaev <ksergy.91@gmail.com>
2015-01-02 02:34:43 +0400

    Start server impl

--- one-to-one/include/Server.hpp
+++ one-to-one/include/Server.hpp
@@ -1,5 +1,13 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include "Message.hpp"
+#include "MessageIO.hpp"
+
+class ServerTCP : virtual public MessageIOTcp {
+};
+
+class ServerUDP : virtual public MessageIOUDP {
+};
 
 #endif // SERVER_HPP
