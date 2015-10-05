75b3c8df54b78e43913097a3e75e321436379a30
Sergy Kanaev <ksergy.91@gmail.com>
2015-10-05 20:56:35 +0300

    Expand thread pool tpl

--- thread-pool/thread-pool.h
+++ thread-pool/thread-pool.h
@@ -1,7 +1,30 @@
 #ifndef _THREAD_POOL_
 #define _THREAD_POOL_
 
+# include "lib.h"
+
+# include <stdbool.h>
+# include <pthread.h>
 /* Thread pool facilities */
+typedef void (*job_function_t)(void *);
 
+typedef struct job_t {
+    list_entry_t le;
+    job_function_t job;
+    void *ctx;
+};
 
-#endif // _THREAD_POOL_
+typedef struct thread_pool {
+    job_t *queue_head;
+    job_t *queue_tail;                                      ///< append jobs here
+    size_t queue_size;
+    mutex job_mutex;                                        ///< queue access mtx
+    semaphore job_semaphore;                                ///< thread run semaphore
+    bool run;                                               ///< should threads run any more
+    pthread_t *thread_id;                                   ///< run thread IDs
+} thread_pool_t;
+
+thread_pool_t *thread_pool_init(size_t thread_count);
+void thread_pool_stop(thread_pool_t *tp);
+
+#endif /* _THREAD_POOL_ */
