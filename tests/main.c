c8b53628361fbc22515d15de4fd5c83232e5592d
Sergy Kanaev <ksergy.91@gmail.com>
2015-10-07 21:06:54 +0300

    add tests

---
+++ tests/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "thread-pool.h"
+#include "lib.h"
+
+#define THREAD_COUNT 10
+#define JOB_COUNT 100
+
+static void r(void *ctx) {
+    fprintf(stderr, "%s: %d\n", __func__, (int)ctx);
+}
+
+int main(void) {
+    thread_pool_t *tp = thread_pool_init(THREAD_COUNT);
+    size_t idx;
+
+    for (idx = 0; idx < JOB_COUNT; ++idx) {
+        thread_pool_post_job(tp, r, (void *)idx);
+    }
+
+    thread_pool_stop(tp, true);
+
+    return 0;
+}
