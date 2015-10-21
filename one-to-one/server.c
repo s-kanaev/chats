bfd3f8c7d0bdf37ead7e821d724de5a222cc6b6b
Sergy Kanaev <ksergy.91@gmail.com>
2015-10-22 00:49:00 +0300

    Add OTO server sender tpl

--- one-to-one/server.c
+++ one-to-one/server.c
@@ -23,14 +23,26 @@ struct oto_server_tcp {
     struct sockaddr_in remote_addr;
     endpoint_socket_t local;
     endpoint_socket_t remote;
+};
 
+struct connection_acceptor {
+    oto_server_tcp_t *server;
     oto_connection_cb_t connection_cb;
     void *connection_ctx;
 };
 
+struct send_buffer {
+    oto_server_tcp_t *server;
+    buffer_t *buffer;
+    size_t bytes_sent;
+    oto_send_cb_t cb;
+    void *ctx;
+};
+
 static
 void oto_tcp_ip4_acceptor(int fd, io_svc_op_t op, void *ctx) {
-    oto_server_tcp_t *server = ctx;
+    struct connection_acceptor *acceptor = ctx;
+    oto_server_tcp_t *server = acceptor->server;
     uint32_t addr;
     socklen_t len;
 
@@ -54,15 +66,42 @@ void oto_tcp_ip4_acceptor(int fd, io_svc_op_t op, void *ctx) {
     server->remote.ep.ep.ip4.addr[2] = (addr >> 0x08) & 0xff;
     server->remote.ep.ep.ip4.addr[3] = addr & 0xff;
 
-    if (!(*server->connection_cb)(&server->remote.ep,
-                                  errno,
-                                  server->connection_ctx)) {
+    if (!(*acceptor->connection_cb)(&server->remote.ep,
+                                    errno,
+                                    acceptor->connection_ctx)) {
         shutdown(server->remote.skt, SHUT_RDWR);
         close(server->remote.skt);
         server->connected = false;
+        pthread_mutex_unlock(&server->mutex);
+        return;
     }
 
+    deallocate(ctx);
     pthread_mutex_unlock(&server->mutex);
+}
+
+static
+void oto_send_sync(struct send_buffer *sb) {
+    /* TODO */
+#error "Not implemented"
+    oto_server_tcp_t *server;
+    buffer_t *buffer;
+
+    assert(sb != NULL);
+
+    server = sb->server;
+    buffer = sb->buffer;
+    size_t bytes_sent = sb->bytes_sent;
+
+    if (!server->connected) return;
+
+    /* TODO */
+}
+
+static
+void oto_send_async(int fd, io_svc_op_t op, void *ctx) {
+    /* TODO */
+#error "Not implemented"
 }
 
 oto_server_tcp_t *oto_server_tcp_init(io_service_t *svc,
@@ -136,7 +175,10 @@ oto_server_tcp_t *oto_server_tcp_init(io_service_t *svc,
         break;
     }
 
-    server->local.skt = socket(socket_family, socket_type, 0);
+    server->local.skt = socket(socket_family,
+                               socket_type | SOCK_CLOEXEC /* | SOCK_NONBLOCK */,
+                               0);
+
     if (server->local.skt < 0) goto fail;
 
     if (bind(server->local.skt,
@@ -181,6 +223,7 @@ void oto_server_tcp_disconnect(oto_server_tcp_t *server) {
 
 void oto_server_tcp_listen_sync(oto_server_tcp_t *server,
                                 oto_connection_cb_t cb, void *ctx) {
+    struct connection_acceptor *acceptor;
     if (!server) return;
 
     pthread_mutex_lock(&server->mutex);
@@ -189,15 +232,20 @@ void oto_server_tcp_listen_sync(oto_server_tcp_t *server,
         return;
     }
 
-    server->connection_cb = cb;
-    server->connection_ctx = ctx;
+    acceptor = allocate(sizeof(struct connection_acceptor));
+    assert(acceptor != NULL);
 
-    oto_tcp_ip4_acceptor(server->local.skt, IO_SVC_OP_READ, server);
+    acceptor->server = server;
+    acceptor->connection_cb = cb;
+    acceptor->connection_ctx = ctx;
+
+    oto_tcp_ip4_acceptor(server->local.skt, IO_SVC_OP_READ, acceptor);
     pthread_mutex_unlock(&server->mutex);
 }
 
 void oto_server_tcp_listen_async(oto_server_tcp_t *server,
                                  oto_connection_cb_t cb, void *ctx) {
+    struct connection_acceptor *acceptor;
     if (!server) return;
 
     pthread_mutex_lock(&server->mutex);
@@ -206,15 +254,19 @@ void oto_server_tcp_listen_async(oto_server_tcp_t *server,
         return;
     }
 
-    server->connection_cb = cb;
-    server->connection_ctx = ctx;
+    acceptor = allocate(sizeof(struct connection_acceptor));
+    assert(acceptor != NULL);
+
+    acceptor->server = server;
+    acceptor->connection_cb = cb;
+    acceptor->connection_ctx = ctx;
 
     io_service_post_job(server->master,
                         server->local.skt,
                         IO_SVC_OP_READ,
                         true,
                         oto_tcp_ip4_acceptor,
-                        server);
+                        acceptor);
     pthread_mutex_unlock(&server->mutex);
 }
 
@@ -222,9 +274,7 @@ void oto_server_tcp_local_ep(oto_server_tcp_t *server, endpoint_socket_t *ep) {
     if (!server) return;
 
     pthread_mutex_lock(&server->mutex);
-
     memcpy(ep, &server->local, sizeof(*ep));
-
     pthread_mutex_unlock(&server->mutex);
 }
 
@@ -232,10 +282,8 @@ void oto_server_tcp_remote_ep(oto_server_tcp_t *server, endpoint_socket_t *ep) {
     if (!server) return;
 
     pthread_mutex_lock(&server->mutex);
-
     if (server->connected)
         memcpy(ep, &server->remote, sizeof(*ep));
-
     pthread_mutex_unlock(&server->mutex);
 }
 
