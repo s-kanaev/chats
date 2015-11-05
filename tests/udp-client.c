b30a1e54e3b79493c5df0063f3995a13e403325b
Sergy Kanaev <ksergy.91@gmail.com>
2015-11-06 00:52:00 +0300

    Add UDP test-client workspace

---
+++ tests/udp-client.c
@@ -0,0 +1,11 @@
+#include "client/client.h"
+#include "io-service.h"
+#include "memory.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+int main(void) {
+    return 0;
+}
