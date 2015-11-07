306de56de0efc593b3bfae352c4bdb067237eb49
Sergy Kanaev <ksergy.91@gmail.com>
2015-11-07 11:59:20 +0300

    Add SQLite 3.x DB creation script. Add details for p2p-mu protocol.

---
+++ chats/p2p/server/sqlite/db.sql
@@ -0,0 +1,14 @@
+.open /var/chat-p2p-mu/clients.db
+
+BEGIN TRANSACTION;
+
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    nickname CHAR(24),
+    keyword CHAR(128),
+    host TEXT,
+    port INTEGER,
+    UNIQUE(nickname)
+);
+
+COMMIT;
