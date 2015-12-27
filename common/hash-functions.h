c5395c8aa82576edd36bbcb970629b5b4203cc35
Sergy Kanaev <ksergy.91@gmail.com>
2015-12-27 20:20:21 +0300

    Add Pearson's 8-byte hash function

---
+++ common/hash-functions.h
@@ -0,0 +1,9 @@
+#ifndef _CHATS_COMMON_HASH_FUNCTIONS_H_
+# define _CHATS_COMMON_HASH_FUNCTIONS_H_
+
+# include <stddef.h>
+# include <stdbool.h>
+
+long long pearson_hash(void *data, size_t len);
+
+#endif /* _CHATS_COMMON_HASH_FUNCTIONS_H_ */
