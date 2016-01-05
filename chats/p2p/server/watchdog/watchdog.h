53927819eb8418d8a3243ae957d0c27fba86cae5
Sergy Kanaev <ksergy.91@gmail.com>
2016-01-05 16:32:57 +0300

    change watchdog API with watchdog_ prefix

--- chats/p2p/server/watchdog/watchdog.h
+++ chats/p2p/server/watchdog/watchdog.h
@@ -13,10 +13,10 @@ void watchdog_deinit(void);
 void watchdog_run(void);
 
 /* for use by receivers */
-void register_client(const char *nickname, size_t nickname_len,
-                     const char *host, size_t host_len,
-                     const char *port, size_t port_len);
-void deregister_client(const char *nickname, size_t nickname_len);
-void continue_registration(const char *nickname, size_t nickname_len);
+void watchdog_register_client(const char *nickname, size_t nickname_len,
+                              const char *host, size_t host_len,
+                              const char *port, size_t port_len);
+void watchdog_deregister_client(const char *nickname, size_t nickname_len);
+void watchdog_continue_registration(const char *nickname, size_t nickname_len);
 
 #endif /* _P2P_MU_WATCHDOG_H_ */
